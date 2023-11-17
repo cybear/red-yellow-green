7889243e0290671cb18bdd0bec913dc55f9519c0
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2023-11-17 14:00:35 +0100

    Add gradient

--- src/components/Light.tsx
+++ src/components/Light.tsx
@@ -9,13 +9,23 @@ type LightProps = {
 const Light: React.FC<LightProps> = ({ color, isActive, onClick }) => {
   return (
     <svg width="200" height="200" onClick={onClick}>
+      <defs>
+        <linearGradient id="Highlight">
+          <stop stopColor='#fff' offset="0%" />
+          <stop stopColor="#fff" stopOpacity="0" offset="100%" />
+        </linearGradient>
+        <linearGradient id="Shadow">
+          <stop stopColor='#000' stopOpacity="0.7" offset="0%" />
+          <stop stopColor="#000" stopOpacity="1" offset="100%" />
+        </linearGradient>
+      </defs>
       <circle
         cx="100"
         cy="100"
         r="90"
         x="10"
         y="10"
-        fill={isActive ? color : '#000000'}
+        fill={color}
         stroke={'#000000'}
         strokeWidth={5}
       />
@@ -26,10 +36,8 @@ const Light: React.FC<LightProps> = ({ color, isActive, onClick }) => {
         r="90"
         x="10"
         y="10"
-        opacity={0.3}
-        fill={color}
-        stroke={'#000000'}
-        strokeWidth={5}
+        opacity={0.6}
+        fill={isActive ? "url(#Highlight)" : "url(#Shadow)"}
       />
     </svg>
   );
