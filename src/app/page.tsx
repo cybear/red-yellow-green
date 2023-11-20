e6fbfba2b5c258a9b92154064f6f8dd66f213f45
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2023-11-20 13:07:10 +0100

    Tidy up front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import Link from 'next/link'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        Fubar
-      </div>
-    </main>
+    <div>
+      <ul>
+        <li><Link href="/equipments">Equipments</Link></li>
+        <li><Link href="/status/123">Status</Link></li>
+      </ul>
+    </div>
   )
 }
