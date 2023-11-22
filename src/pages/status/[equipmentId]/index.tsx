843609d056442b17272b9783b0d8a278d64e597e
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2023-11-22 21:50:09 +0100

    Add equipment status page to new structure

---
+++ src/pages/status/[equipmentId]/index.tsx
@@ -0,0 +1,45 @@
+/*
+This is an equipment page.
+It is shown on a screen by each equipment.
+It shows the status of the specific equipment.
+Workers can change the status.
+Status states:
+- If it's red (off), it can be changed to yellow (warming up) or green (working).
+- If it's yellow, it can be changed to green (working) or red (off).
+- If it's green, it can be changed to yellow (cooling down) or red (off).
+
+Orders:
+When the status changes to green, it means the equipment has started working on a new order.
+When the status changes from green to yellow, we can assume that the work is completed.
+
+*/
+'use client'
+import Light from "@/components/Light";
+import { useParams } from "next/navigation";
+import { useRouter } from "next/router";
+import React from "react";
+
+const statusColors = ['red', 'yellow', 'green'];
+
+const Status: React.FC = () => {
+  const [activeColor, setActiveColor] = React.useState('red');
+  const router = useRouter();
+  const equipmentId = router.query.equipmentId;
+
+  return (
+    <div>
+      {statusColors
+        .map((color) =>
+          <div key={color} onClick={() => setActiveColor(color)}>
+            <Light
+              color={color}
+              isActive={color === activeColor}
+            />
+          </div>
+        )}
+        <p>Client ID: {equipmentId}</p>
+    </div>
+  );
+};
+
+export default Status;
