4a654d5a5cc2edf0f9200896ce400236a35c11c1
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2023-11-23 07:10:09 +0100

    Change to scheduled and history

--- src/pages/orders/[equipmentId]/index.tsx
+++ src/pages/orders/[equipmentId]/index.tsx
@@ -11,41 +11,43 @@ Restrictions: Only to be available to Supervisors.
 import { useRouter } from 'next/router';
 
 const EquipmentOrders: React.FC = () => {
-  const orders = [
+  const ordersHistory = [
     {
       orderId: 'Order 1',
-      status: 'green',
     },
     {
       orderId: 'Order 2',
-      status: 'green',
     },
+  ];
+  const scheduledOrders = [
     {
       orderId: 'Order 3',
     },
     {
       orderId: 'Order 4',
     },
-
   ];
+
   const router = useRouter();
   return (
     <>
       <h1>Equipment Orders for {router.query.equipmentId}</h1>
-      {orders.map((order) => (
-        <div key={order.orderId}>
-          <h2>{order.orderId}</h2>
-          <p>Status: {
-            order.status
-              ? <span style={{ backgroundColor: order.status }}>{order.status}</span>
-              : 'Queued'
-          }</p>
-          <p>Order: <span>{order.orderId}</span></p>
-        </div>
-      ))}
+      <form>
+        <h2>Add a new order</h2>
+        <input type="text" id="orderId" name="orderId" placeholder='Order ID' />
+        <button type="submit">Add to queue</button>
+      </form>
+      <hr />
+      <div>
+          <h2>Scheduled orders</h2>
+          {scheduledOrders.map((order) => (
+            <div key={order.orderId}>
+              <p>Order: <span>{order.orderId}</span></p>
+            </div>
+          ))}
+      </div>
     </>
   );
 }
-
 
 export default EquipmentOrders;
