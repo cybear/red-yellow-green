85638a92ae1a3d1fcd488cafc39a8829de13f92d
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2023-11-23 14:18:47 +0100

    Add polling for Dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,15 +5,19 @@ It shows the current status and order of all equipments.
 import Light from "@/components/Light";
 import { Equipment } from "@/utils/equipmentData";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Dashboard: React.FC = () => {
   const [data, setData] = useState<Array<Equipment>>([]);
+  const timerIdRef = useRef<ReturnType<typeof setInterval> | null>(null);
   useEffect(() => {
-    fetch('http://localhost:3000/api/equipments')
-      .then(res => res.json())
-      .then(data => 
-      setData(data));
+    const fetcher = () => fetch('http://localhost:3000/api/equipments')
+    .then(res => res.json())
+    .then(data => setData(data));
+    timerIdRef.current = setInterval(fetcher, 1000);
+    return () => {
+      timerIdRef.current && clearInterval(timerIdRef.current);
+    }
   }, []);
 
   return (
@@ -22,8 +26,8 @@ const Dashboard: React.FC = () => {
       {data.map(({ id, currentColor, currentStatus, currentOrder }) => (
       <div key={id}>
         <h2><Light color={currentColor} isActive width={20} height={20} /> <Link href={`/status/${id}`}>{id}</Link></h2>
-        <p>Status: {currentStatus}</p>
-        <p>Order: <span style={{color: currentColor}}>{currentOrder?.orderId}</span></p>
+        <p><Link href={`/status/${id}`}>Status</Link>: {currentStatus}</p>
+        <p><Link href={`/orders/${id}`}>Order</Link>: <span style={{color: currentColor}}>{currentOrder?.orderId}</span></p>
       </div>
       ))}
     </div>
