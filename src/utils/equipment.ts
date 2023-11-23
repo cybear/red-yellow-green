4ffd8036e8f952820a5c1ee34e004e7dde189cce
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2023-11-23 07:10:01 +0100

    Add data generator for transparency

--- src/utils/equipment.ts
+++ src/utils/equipment.ts
@@ -1,25 +1,85 @@
-export type Status = 'red' | 'yellow' | 'green';
-export type Order = string;
+export type Status = {
+  color: 'red' | 'yellow' | 'green';
+  text: 'Standing still' | 'Starting up' | 'Winding down' | 'Producing';
+  timestamp: number;
+};
+
+export type Order = {
+  orderId: string;
+  timestamp: number;
+};
+
 export type Equipment = {
   equipmentId: string;
-  status: Status[];
+  statuses: Status[];
   orders: Order[];
 };
 
-const randomStatus = (): Status => Math.random() < 1 / 3 ? 'red' : Math.random() < 2 / 3 ? 'yellow' : 'green';
-const randomOrder = () => `Brick ${~~(Math.random() * 100)}`
+const generateNewStatus = (oldStatus: Status): Status => {
+  const getNewColor = (oldColor: Status['color']) => {
+    switch (oldColor) {
+      case 'green': return Math.random() < 0.5 ? 'yellow' : 'red';
+      case 'yellow': return Math.random() < 0.5 ? 'green' : 'red';
+      case 'red': return Math.random() < 0.5 ? 'green' : 'yellow';
+      default: throw new Error('Invalid color');
+    };
+  };
+  const getNewStatus = (oldColor: Status['color'], newColor: Status['color']) => {
+    if (newColor === 'green') return 'Producing';
+    if (newColor === 'red') return 'Standing still';
+    if (newColor === 'yellow' && oldColor === 'green') return 'Winding down';
+    if (newColor === 'yellow' && oldColor === 'red') return 'Starting up';
+    throw new Error('Invalid color combo');
+  };
+  const color = getNewColor(oldStatus.color);
+  const text = getNewStatus(oldStatus.color, color);
+  const timestamp = oldStatus.timestamp + Math.random() * 1000 * 60 * 60;
+  return {
+    color,
+    text,
+    timestamp
+  };
+};
 
-export const randomEquipment = (i: number) => ({
-  equipmentId: i.toString(),
-  status: Array(1 + ~~(Math.random() * 5)).fill(0).map(randomStatus),
-  orders: Array(1 + ~~(Math.random() * 5)).fill(0).map(randomOrder),
-});
+const randomOrder = (oldOrder: Order): Order => {
+  const orderId = `Order ${~~(Math.random() * 10000)}`;
+  const timestamp = oldOrder.timestamp;
+  return {
+    orderId,
+    timestamp
+  };
+}
 
-const equipmentList: Array<{
-  equipmentId: string;
-  status: Array<Status>;
-  orders: Array<Order>;
-}> = [];
+export const randomEquipment = (i: number) => {
+  const amountOrders = 1 + ~~(Math.random() * 5);
+  const orders = Array(amountOrders).fill(0).reduce((acc) => {
+    const lastOrder = acc[acc.length - 1];
+    const newOrder = randomOrder(lastOrder);
+    return [...acc, newOrder];
+  }, [{
+    orderId: 'Order 0',
+    timestamp: Date.now(),
+  }]);
+
+  const amountStatus = 1 + ~~(Math.random() * 5);
+  const statuses: Equipment['statuses'] = Array(amountStatus).fill(0).reduce((acc) => {
+    const lastStatus = acc[acc.length - 1];
+    const newStatus = generateNewStatus(lastStatus)
+    return [...acc, newStatus];
+  }, [{
+    color: 'red',
+    text: 'Standing still',
+    timestamp: Date.now(),
+  }]);
+
+  return ({
+    equipmentId: i.toString(),
+    statuses,
+    orders,
+  });
+};
+
+const equipmentList: Array<Equipment> = [];
 
 for (let i = 0; i < 20; i++) {
   equipmentList.push(randomEquipment(i));
